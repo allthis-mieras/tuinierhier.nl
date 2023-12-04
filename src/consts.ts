5a97ee7aa4b9aec68c85321371b9872e30afee95
mieras <mieras@gmail.com>
2023-12-04 16:44:56 +0100

    yo

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Tuinierhier • Eefje Peddemors';
+export const SITE_DESCRIPTION = 'Tuinfluencer, plantlover, kweekbeest, tuintijger, gardenista.';
+export const NAME_REAL = 'Eefje Peddemors';
+export const NAME = 'Tuinierhier';
